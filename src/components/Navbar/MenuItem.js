abfe652fb4297b24e0947b8f9778ecc2a3578203
Rohan Thapa <072bct529@pcampus.edu.np>
2021-04-23 17:01:00 +0545

    responsive navabar done

---
+++ src/components/Navbar/MenuItem.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Booking Status',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Browse',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Register',
+        url: '#',
+        cName:'nav-links-mobile'
+    }
+]
