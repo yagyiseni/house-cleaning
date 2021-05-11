a84aad69507204bb607d5794ee1e51533bcff195
Rohan Thapa <072bct529@pcampus.edu.np>
2021-05-11 09:05:57 +0545

    router modification

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { CleanerProvider } from './context';
 ReactDOM.render(
   <React.StrictMode>
