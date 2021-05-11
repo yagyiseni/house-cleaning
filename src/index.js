b1599cd0ccdd6563e92511237e96b7409f3a78a2
Rohan Thapa <072bct529@pcampus.edu.np>
2021-05-11 09:28:07 +0545

    hello

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { CleanerProvider } from './context';
 ReactDOM.render(
   <React.StrictMode>
     <CleanerProvider>
-    <Router>
+    <Router basename="/WeClean">
       <App />
     </Router>
     </CleanerProvider>
